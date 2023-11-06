517b3b130f9e60b178b182f578c18eb9906eff22
Chukwura Desmond <122203052+desmondsama@users.noreply.github.com>
2023-11-06 10:11:10 +0100

    Create request 1.sql

---
+++ SQL codes/request 1.sql
@@ -0,0 +1,9 @@
+/*Generate an aggregated monthly sales report for Croma customer in fiscal year 2021 
+*/
+SELECT s.date, s.product_code, p.product, p.variant, s.sold_quantity 
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+        ON s.product_code=p.product_code
+	WHERE 
+            customer_code=90002002 AND 
+    	    get_fiscal_year(date)=2021  
